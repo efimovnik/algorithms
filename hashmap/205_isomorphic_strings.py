c6d8c6815864f55bce501ddfe519b373593c4c97
Nik Efimov <efimov.nikolai.ig@gmail.com>
2024-12-17 08:49:02 -0700

    solution from 290 serves 205 as well

--- hashmap/205_isomorphic_strings.py
+++ hashmap/205_isomorphic_strings.py
@@ -42,3 +42,8 @@ class Solution:
 
 # time: O(n)
 # space: O(n)
+
+# Beautiful one-line soultion with sets!
+
+def isIsomorphic(self, s: str, t: str) -> bool:
+        return len(set(s)) == len(set(t)) == len(set(zip(s,t)))
